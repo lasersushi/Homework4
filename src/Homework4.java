a5d7c29ee5d61e70368b0a750adc4437fa36ee37
lasersushi <lwalker30@priorypanther.com>
2026-09-12 16:08:21 -0700

    Imporve error messaging, write main, make opposite function

--- src/Homework4.java
+++ src/Homework4.java
@@ -5,11 +5,18 @@ Mr. Menezes
 */
 
 public class Homework4 {
-    public static void main(String[] args) {
+    private final static String ArgErrorMessage = "Incorrect argument parameters";
 
+    public static void main(String[] args) {
+        String operation = args[0];
+        String a = args[1];
+        String b = args[2];
+        String opposite = args[3];
+        calculate(operation, a, b, opposite);
     }
 
     public static void calculate(String operation, String a, String b, String oppositeOrder) {
+        boolean isOp = isOpposite(oppositeOrder);
         boolean aIsInt = CheckInt(a);
         boolean bIsInt = CheckInt(b);
         boolean doubleOp = true;
@@ -18,10 +25,17 @@ public class Homework4 {
         if (bIsInt && aIsInt) {
             int aInt = (int) (aDouble);
             int bInt = (int) (bDouble);
-            computeInt(operation, aInt, bInt);
+            System.out.println(computeInt(operation, aInt, bInt));
             doubleOp = false;
         } else {
-            computeDouble(operation, aDouble, bDouble);
+            System.out.println(computeDouble(operation, aDouble, bDouble));
+        }
+        if (isOp && doubleOp) {
+            System.out.println(computeDouble(operation, bDouble, aDouble));
+        } else if (isOp && doubleOp == false) {
+            int aIntTwo = (int) (aDouble);
+            int bIntTwo = (int) (bDouble);
+            System.out.println(computeInt(operation, bIntTwo, aIntTwo));
         }
     }
 
@@ -32,6 +46,11 @@ public class Homework4 {
             ans = true;
         }
         return ans;
+    }
+
+    public static boolean isOpposite(String opOrder) {
+        boolean isOp = opOrder.equals("yes");
+        return isOp;
     }
 
     public static int computeInt(String op, int a, int b) {
@@ -45,7 +64,7 @@ public class Homework4 {
         } else if (op.equals("multiply")) {
             ans = a * b;
         } else {
-            throw new IllegalArgumentException("Incorrect argument parameters");
+            throw new IllegalArgumentException(ArgErrorMessage);
         }
         return ans;
     }
@@ -61,7 +80,7 @@ public class Homework4 {
         } else if (op.equals("multiply")) {
             ans = a * b;
         } else {
-            throw new IllegalArgumentException("Incorrect argument parameters");
+            throw new IllegalArgumentException(ArgErrorMessage);
         }
         return ans;
     }
