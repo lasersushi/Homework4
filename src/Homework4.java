9aa18dc26c544c6f1bf801bdcd8792c3c97df514
lasersushi <lwalker30@priorypanther.com>
2026-09-14 17:09:52 -0700

    Commenting

--- src/Homework4.java
+++ src/Homework4.java
@@ -5,14 +5,16 @@ Mr. Menezes
 */
 
 public class Homework4 {
-    private final static String ArgErrorMessage = "Incorrect argument parameters";
+    private final static String ArgErrorMessage = "Incorrect argument parameters"; // Error message which is thrown when
+                                                                                   // you have inccorect arguement
+                                                                                   // params
 
     public static void main(String[] args) {
         String operation = args[0];
         String a = args[1];
         String b = args[2];
-        String opposite = args[3];
-        calculate(operation, a, b, opposite);
+        String opposite = args[3]; // Takes in the arguements
+        calculate(operation, a, b, opposite); // Calls the calculate method
     }
 
     public static void calculate(String operation, String a, String b, String oppositeOrder) {
@@ -22,15 +24,15 @@ public class Homework4 {
         boolean doubleOp = true;
         double aDouble = Double.parseDouble(a);
         double bDouble = Double.parseDouble(b);
-        if (bIsInt && aIsInt) {
+        if (bIsInt && aIsInt) { // Converts the doubles to ints
             int aInt = (int) (aDouble);
             int bInt = (int) (bDouble);
             System.out.println(computeInt(operation, aInt, bInt));
             doubleOp = false;
-        } else {
+        } else { // If it isn't an int then just do the operation with doubles
             System.out.println(computeDouble(operation, aDouble, bDouble));
         }
-        if (isOp && doubleOp) {
+        if (isOp && doubleOp) { // Checks if it is opposite and also prints the opposite one
             System.out.println(computeDouble(operation, bDouble, aDouble));
         } else if (isOp && doubleOp == false) {
             int aIntTwo = (int) (aDouble);
@@ -41,18 +43,19 @@ public class Homework4 {
 
     public static boolean CheckInt(String a) {
         boolean ans = false;
-        if (a.indexOf(".") == -1) {
+        if (a.indexOf(".") == -1) { // This method checks if the iputed values are integers
             ans = true;
         }
         return ans;
     }
 
     public static boolean isOpposite(String opOrder) {
-        boolean isOp = opOrder.equals("yes");
+        boolean isOp = opOrder.equals("yes"); // This method checks if oppositeoder is on
+
         return isOp;
     }
 
-    public static int computeInt(String op, int a, int b) {
+    public static int computeInt(String op, int a, int b) { // This method does the assigned operation.
         int ans = 0;
         if (op.equals("add")) {
             ans = a + b;
@@ -68,7 +71,8 @@ public class Homework4 {
         return ans;
     }
 
-    public static double computeDouble(String op, double a, double b) {
+    public static double computeDouble(String op, double a, double b) { // This method also does the assigned operation
+                                                                        // but for doubles
         double ans = 0;
         if (op.equals("add")) {
             ans = a + b;
